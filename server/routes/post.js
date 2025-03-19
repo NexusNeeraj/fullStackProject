9b0889608c3a55f16828b67c11d7230115555792
Neeraj Gupta <na1993194@gmail.com>
2025-03-20 03:27:07 +0530

    update:auth-middleware-added

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { createPost, getAllPosts, updatePost, deletePost } = require("../controllers/postController");
+const authMiddleware = require('../middlewares/authMiddleware');
 
-
-router.get('/posts', getAllPosts);
-router.post('/create', createPost);
-router.put('/posts/:id', updatePost);
-router.delete('/posts/:id', deletePost);
+router.get('/posts', authMiddleware, getAllPosts);
+router.post('/create', authMiddleware, createPost);
+router.put('/posts/:id', authMiddleware, updatePost);
+router.delete('/posts/:id', authMiddleware, deletePost);
 
 
 module.exports = router;
